d61af1f7c5ce3e97aab2980e031f2d249a29d685
BaconStrps <jaredcochran@outlook.com>
2021-08-19 18:26:44 -0400

    Added Trr.cpp to genlib.sh

--- genlib.sh
+++ genlib.sh
@@ -3,4 +3,6 @@
 gcc -c Index.cpp -lstdc++
 gcc -c Gro.cpp -lstdc++
 gcc -c Topol.cpp -lstdc++
-ar rvs -o libgfile.a Gro.o Index.o Topol.o
+gcc -c Trr.cpp -lstdc++
+
+ar rvs -o libgfile.a Gro.o Index.o Topol.o Trajdump.o
